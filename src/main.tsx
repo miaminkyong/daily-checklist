f14cd62312623a00a7ad2fdfbea8d70cca200053
miaminkyong <mia.minkyong@gmail.com>
2026-07-14 11:32:35 +0900

    Initialize main.tsx with React setup

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.tsx'
+import './index.css' // 디자인 파일 불러오기
+
+ReactDOM.createRoot(document.getElementById('root')!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
